9aededb6cf345a8576524b40390a01a179005b7f
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2017-10-04 08:57:15 -0700

    updating singer_locations documentation

--- R/singer_locations.R
+++ R/singer_locations.R
@@ -3,6 +3,14 @@
 #' @format A \code{data.frame} with 10100 observations and 14 columns. The columns are defined as follows:
 #' \describe{
 #'  \item{\code{track_id}}{unique ID for each track}
+#'  \item{\code{title}}{song title}
+#'  \item{\code{song_id}}{The Echo Nest song ID, note that a song can be associated with many tracks (with very slight audio differences)}
+#'  \item{\code{duration}}{duration of the track in seconds}
+#'  \item{\code{release}}{album name from which the track was taken, some songs / tracks can come from many albums, only one is given}
+#'  \item{\code{artist_id}}{the musicbrainz.org ID for the artist}
+#'  \item{\code{artist_name}}{artist name}
+#'  \item{\code{year}}{year when the song was released, according to musicbrainz.org}
+#'  \item{\code{artist_hotttnesss}}{according to The Echo Nest, when downloaded, artist 'hotttnesss' of 0.8 (on a scale of 0 and 1)}
 #' }
 #' @source Million Songs Dataset
 #' @author JB
