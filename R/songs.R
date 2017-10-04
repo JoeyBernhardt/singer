13fed65c915416ee029a8185bcf2fd7b3a6ec487
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2017-10-03 19:43:11 -0700

    add documentation for songs and locations

---
+++ R/songs.R
@@ -0,0 +1,14 @@
+#' @title songs
+#' @description This data set contains a subset of song data
+#' @format A \code{data.frame} with 10100 observations and 14 columns. The columns are defined as follows:
+#' \describe{
+#'  \item{\code{track_id}}{unique ID for each track}
+#' }
+#' @source Million Songs Dataset
+#' @author JB
+#' @examples
+#' \dontrun{
+#' data("songs")
+#' summary(songs)
+#' }
+"songs"
